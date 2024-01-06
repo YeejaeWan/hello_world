60976ca3bfbcd16604523aa7cebc5aec05d7f046
YeeeeJaeWan <ejw04199@naver.com>
2024-01-06 20:30:21 +0900

    debug 로고 없애기 및 배경색 변경

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
   );
@@ -12,12 +13,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange,
+      backgroundColor: Color(0xFFF99231),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset(
-
               'asset/img/logo.png'
           ),
           CircularProgressIndicator(
